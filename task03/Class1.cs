7ea7f0c79c362fee2410058d9b5e60207bc74935
DanilAsafov <perez3498145@gmail.com>
2025-06-28 01:07:57 +0600

    feat: Implementation of iterators
    - Created CustomCollection<T> class implements IEnumerable<T>
    
    - Implemented IEnumerable GetReverseEnumerator, GenerateSequence and FilterAndSort methods
    
    - Added 4 xUnit test cases

---
+++ task03/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace task03;
+
+public class CustomCollection<T> : IEnumerable<T> {
+    private readonly List<T> _items = new();
+
+    public void Add(T item) => _items.Add(item);
+    public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public IEnumerable<T> GetReverseEnumerator() {
+        foreach (var item in Enumerable.Range(0, _items.Count))
+            yield return _items[_items.Count - 1 - item];
+    }
+
+    public static IEnumerable<int> GenerateSequence(int start, int count) {
+        foreach (var i in Enumerable.Range(0, count))
+            yield return start + i;
+    }
+
+    public IEnumerable<T> FilterAndSort(Func<T, bool> predicate, Func<T, IComparable> keySelector) {
+        return _items.Where(predicate).OrderBy(keySelector);
+    }
+}
